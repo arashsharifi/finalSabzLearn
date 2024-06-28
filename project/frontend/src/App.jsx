d7c4e9b3238a837f02293115af05fd66b739dfa8
arash amirsharifi <arashamirsharifi1970@gmail.com>
2024-06-28 19:46:47 +0330

    feat: context api auth login and register users

--- project/frontend/src/App.jsx
+++ project/frontend/src/App.jsx
@@ -1,12 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { useRoutes } from "react-router-dom";
+
 import routes from "./routes";
+import AuthContext from "./context/authContext";
 
 export default function App() {
+  const [isLoggedIn, SetIsLoggedIn] = useState(false);
+  const [token, setToken] = useState(false);
+  const [userInfos, setUserInfos] = useState({});
   const router = useRoutes(routes);
+
+  const login = (userInfos, token) => {
+    console.log("token", token);
+    console.log("userInfos", userInfos);
+    setToken(token);
+    SetIsLoggedIn(true);
+    setUserInfos(userInfos);
+    localStorage.setItem("user", JSON.stringify({ token }));
+  };
+  // console.log("userInfos", userInfos);
+  const logout = () => {
+    setToken(null);
+    setUserInfos({});
+    localStorage.removeItem("user");
+  };
   return (
-    <>
-      <div className="">{router}</div>
-    </>
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        token: token,
+        userInfos: userInfos,
+        login: login,
+        logout: logout,
+      }}
+    >
+      <div>{router}</div>
+    </AuthContext.Provider>
   );
 }
