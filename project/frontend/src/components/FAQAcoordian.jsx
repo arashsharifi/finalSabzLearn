a3584f6e7e4f155ad89efd3bc3a4c7c569059c3b
arash amirsharifi <arashamirsharifi1970@gmail.com>
2024-07-19 19:08:28 +0330

    add: section comment component fix: accordian  whith data fix :courseinfo

--- project/frontend/src/components/FAQAcoordian.jsx
+++ project/frontend/src/components/FAQAcoordian.jsx
@@ -1,21 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Accordian from "./Accordian";
 import { AccordionData } from "../data";
 
-export default function FAQAcoordian() {
-  const [datas, setDatas] = useState(AccordionData);
+export default function FAQAcoordian({ sessions }) {
+  useEffect(() => {
+    console.log("sessions", sessions);
+  }, [sessions]);
+
+  console.log(sessions.length);
 
   return (
     <div className=" p-4 bg-gray-200 rounded-lg w-full flex flex-col gap-2 bg-grey">
-      {datas.map((data) => (
-        <Accordian
-          key={data.id}
-          title={data.title}
-          desc={data.desc}
-          time={data.time}
-        />
-      ))}
+      {sessions.length === 0 ? (
+        <p className="text-center text-error font-bold text-xl">
+          جلسه‌ای برای این دوره در نظر گرفته نشده است.
+        </p>
+      ) : (
+        sessions.map((data, index) => (
+          <Accordian
+            key={data._id}
+            title={data.title}
+            time={data.time}
+            index={index}
+          />
+        ))
+      )}
     </div>
   );
 }
