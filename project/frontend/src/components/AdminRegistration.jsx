d902ecc4428cbf20261dd0a3a381ce776e915856
m.elyasi <maahsa.elyasi@gmail.com>
2024-11-05 13:06:10 +0330

    add  users by adminpanel

---
+++ project/frontend/src/components/AdminRegistration.jsx
@@ -0,0 +1,256 @@
+import React, { useContext, useState } from "react";
+import {
+  emailValidator,
+  maxValidator,
+  minValidator,
+  requiredValidator,
+} from "../validators/rules";
+import AuthContext from "../context/authContext";
+import InputTextArea from "./UI/InputTextArea";
+import { useForm } from "../hooks/useForm";
+import { toast } from "react-toastify"; 
+
+export default function AdminRegistration({onUserAdded }) {
+  const [gender, setGender] = useState("");
+
+  const handleChange = (event) => {
+    setGender(event.target.value);
+  };
+
+  const authContext = useContext(AuthContext);
+
+  const [formState, onInputsHandler] = useForm(
+    {
+      username: {
+        value: "",
+        isValid: false,
+      },
+      name: {
+        value: "",
+        isValid: false,
+      },
+      email: {
+        value: "",
+        isValid: false,
+      },
+      password: {
+        value: "",
+        isValid: false,
+      },
+      phoneNumber: {
+        value: "",
+        isValid: false,
+      },
+    },
+    false
+  );
+
+  const registaerAdmimSubmitHandler = async (e) => {
+    e.preventDefault();
+    const newUserInfos = {
+      name: formState.inputs.name.value,
+      username: formState.inputs.username.value,
+      email: formState.inputs.email.value,
+      password: formState.inputs.password.value,
+      confirmPassword: formState.inputs.password.value,
+      phone: formState.inputs.phoneNumber.value,
+    };
+
+    try {
+      const response = await fetch("http://localhost:4000/v1/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newUserInfos),
+      });
+
+      if (response.ok) {
+        const result = await response.json();
+        onUserAdded()
+        toast.success(`${result.user.name} 🎈✨ کاربر با موفقیت ثبت شد`, {
+          position: "top-center",
+          autoClose: 2000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          theme: "light",
+        });
+      } else {
+        const errorData = await response.json();
+        console.log("errorData", errorData.message);
+        console.error(errorData);
+
+        if (errorData.message === "this phone number banned!") {
+          toast.error("این کاربر بند شده است", {
+            position: "top-right",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            theme: "light",
+          });
+        } else {
+          toast.error(`${errorData.message[0].message}`, {
+            position: "top-right",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            theme: "light",
+          });
+        }
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="flex flex-col gap-2 items-center justify-center p-2 rounded-sm shadow-xl m-4 font-iransans max-w-[100%] md:max-w-[70%] border border-grey">
+      <p className="text-xl font-bold italic self-start">ثبت نام کاربران</p>
+      <div className="flex w-full">
+        <form action="#" className="w-full flex flex-col gap-4 mt-5">
+          <div className="flex flex-col md:flex-row gap-2">
+            <InputTextArea
+              id="name"
+              className="flex items-center gap-2 w-[95%] mx-auto p-2 border bg-myWhite rounded-md shadow-md shadow-greydark"
+              element="input"
+              type="text"
+              icons="family"
+              validations={[
+                requiredValidator(),
+                minValidator(10),
+                maxValidator(20),
+              ]}
+              placeholder="نام نام خانوادگی"
+              onInputsHandler={onInputsHandler}
+            />
+            <InputTextArea
+              id="username"
+              className="flex items-center gap-2 w-[95%] mx-auto p-2 border bg-myWhite rounded-md shadow-md shadow-greydark"
+              element="input"
+              type="text"
+              icons="person"
+              validations={[
+                requiredValidator(),
+                minValidator(6),
+                maxValidator(20),
+              ]}
+              placeholder="نام کاربری"
+              onInputsHandler={onInputsHandler}
+            />
+          </div>
+          <div className="flex flex-col md:flex-row gap-2">
+            <InputTextArea
+              id="phoneNumber"
+              className="flex items-center gap-2 w-[95%] mx-auto p-2 border bg-myWhite rounded-md shadow-md shadow-greydark"
+              element="input"
+              type="text"
+              icons="phone"
+              validations={[
+                requiredValidator(),
+                minValidator(10),
+                maxValidator(12),
+              ]}
+              placeholder="شماره تماس"
+              onInputsHandler={onInputsHandler}
+            />
+            <InputTextArea
+              id="email"
+              className="flex items-center gap-2 w-[95%] mx-auto p-2 border bg-myWhite rounded-md shadow-md shadow-greydark"
+              element="input"
+              type="email"
+              validations={[
+                requiredValidator(),
+                minValidator(4),
+                emailValidator(),
+              ]}
+              icons="email"
+              placeholder="ایمیل را وارد نمایید"
+              onInputsHandler={onInputsHandler}
+            />
+          </div>
+          <div className="flex justify-start mr-3">
+            <InputTextArea
+              id="password"
+              className="flex items-center gap-2 w-[97%] md:w-[48%] p-2 border bg-myWhite rounded-md shadow-md shadow-greydark self-end"
+              element="input"
+              type="password"
+              icons="lock"
+              placeholder="رمز عبور"
+              validations={[
+                requiredValidator(),
+                minValidator(6),
+                maxValidator(11),
+              ]}
+              onInputsHandler={onInputsHandler}
+            />
+          </div>
+          <div className="flex w-full justify-end p-2 bg-customThree">
+            <button
+              type="submit"
+              onClick={registaerAdmimSubmitHandler}
+              disabled={!formState.isFormValid}
+              className={`${
+                formState.isFormValid
+                  ? "bg-success hover:bg-greentoond h-[40px]"
+                  : "bg-greydoubledarko cursor-not-allowed h-[40px]"
+              } text-white w-[30%] text-xl m-2 rounded-md duration-200 shadow-md`}
+            >
+              ثبت نام
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+
+
+
+{/* <div className="flex flex-col gap-1">
+<h2 className="text-lg font-semibold mb-4">انتخاب جنسیت</h2>
+<div className="flex gap-2">
+  <div className="flex gap-2 items-center">
+    <input
+      type="radio"
+      name="gender"
+      value="male"
+      checked={gender === "male"}
+      onChange={handleChange}
+      required
+      className="appearance-none h-5 w-5 border border-customfour rounded-full checked:bg-customtwo checked:border-transparent focus:outline-none focus:ring-2 focus:ring-customfour hover:ring-2 hover:ring-customtwo transition duration-200"
+    />
+    <label className="text-gray-700 ml-2">مرد</label>
+  </div>
+  <div className="flex gap-2 items-center">
+    <input
+      type="radio"
+      name="gender"
+      value="female"
+      checked={gender === "female"}
+      onChange={handleChange}
+      required
+      className="appearance-none h-5 w-5 border border-customfour rounded-full checked:bg-customtwo checked:border-transparent focus:outline-none focus:ring-2 focus:ring-customfour hover:ring-2 hover:ring-customtwo transition duration-200"
+    />
+    <label className="text-gray-700 ml-2">زن</label>
+  </div>
+  <div className="flex gap-2 items-center">
+    <input
+      type="radio"
+      name="gender"
+      value="other"
+      checked={gender === "other"}
+      onChange={handleChange}
+      required
+      className="appearance-none h-5 w-5 border border-customfour rounded-full checked:bg-customtwo checked:border-transparent focus:outline-none focus:ring-2 focus:ring-customfour hover:ring-2 hover:ring-customtwo transition duration-200"
+    />
+    <label className="text-gray-700 ml-2">متفرق</label>
+  </div>
+</div>
+</div> */}
--- project/frontend/src/pages/panelAdmin/Users.jsx
+++ project/frontend/src/pages/panelAdmin/Users.jsx
@@ -2,33 +2,14 @@
 import React, { useEffect, useState } from "react";
 import MaterialTable from "../../components/UI/MaterialTable";
 import swal from "sweetalert";
+import AdminRegistration from "../../components/AdminRegistration";
 export default function Users() {
   const [token, setToken] = useState(false);
   const [users, setUsers] = useState([]);
   const [bannedUsers, setBannedUsers] = useState([]);
   // const [removeId,setRemoveId]=useState(null)
 
-  function getAllUsers() {
-    const localStorageData = JSON.parse(localStorage.getItem("user"));
-
-    fetch("http://localhost:4000/v1/users", {
-      headers: {
-        Authorization: `Bearer ${localStorageData.token}`,
-      },
-    })
-      .then((res) => res.json())
-      .then((result) => {
-        const filteredUsers = result
-          .filter((user) => user.role !== "ADMIN")
-          .map((user) => ({
-            id: user?._id,
-            name: user?.name,
-            email: user?.email,
-            username: user?.username,
-          }));
-        setUsers(filteredUsers);
-      });
-  }
+  
 
   const handleDeleteClick = async (userId) => {
     // نمایش پنجره تایید با استفاده از SweetAlert و دریافت نتیجه با await
@@ -122,28 +103,78 @@ export default function Users() {
     }
   };
 
-  useEffect(() => {
+  // function getAllUsers() {
+  //   const localStorageData = JSON.parse(localStorage.getItem("user"));
+
+  //   fetch("http://localhost:4000/v1/users", {
+  //     headers: {
+  //       Authorization: `Bearer ${localStorageData.token}`,
+  //     },
+  //   })
+  //     .then((res) => res.json())
+  //     .then((result) => {
+  //       const filteredUsers = result
+  //         .filter((user) => user.role !== "ADMIN")
+  //         .map((user) => ({
+  //           id: user?._id,
+  //           name: user?.name,
+  //           email: user?.email,
+  //           username: user?.username,
+  //         }));
+  //       setUsers(filteredUsers);
+  //     });
+  // }
+
+  // useEffect(() => {
+  //   const localStorageData = JSON.parse(localStorage.getItem("user"));
+  //   if (localStorageData) {
+  //     fetch("http://localhost:4000/v1/users", {
+  //       headers: {
+  //         Authorization: `Bearer ${localStorageData.token}`,
+  //       },
+  //     })
+  //       .then((res) => res.json())
+  //       .then((result) => {
+  //         const filteredUsers = result
+  //           .filter((user) => user.role !== "ADMIN")
+  //           .map((user) => ({
+  //             id: user?._id,
+  //             name: user?.name,
+  //             email: user?.email,
+  //             username: user?.username,
+  //             phone: user?.phone,
+  //           }));
+  //         setUsers(filteredUsers);
+  //       });
+  //   }
+  // }, [token]);
+
+  const getAllUsers = async () => {
     const localStorageData = JSON.parse(localStorage.getItem("user"));
-    if (localStorageData) {
-      fetch("http://localhost:4000/v1/users", {
-        headers: {
-          Authorization: `Bearer ${localStorageData.token}`,
-        },
-      })
-        .then((res) => res.json())
-        .then((result) => {
-          const filteredUsers = result
-            .filter((user) => user.role !== "ADMIN")
-            .map((user) => ({
-              id: user?._id,
-              name: user?.name,
-              email: user?.email,
-              username: user?.username,
-              phone: user?.phone,
-            }));
-          setUsers(filteredUsers);
-        });
-    }
+
+    const response = await fetch("http://localhost:4000/v1/users", {
+      headers: {
+        Authorization: `Bearer ${localStorageData.token}`,
+      },
+    });
+
+    const result = await response.json();
+    const filteredUsers = result
+      .filter((user) => user.role !== "ADMIN")
+      .map((user) => ({
+        id: user?._id,
+        name: user?.name,
+        email: user?.email,
+        username: user?.username,
+        phone: user?.phone,
+      }));
+
+    setUsers(filteredUsers);
+  };
+
+
+  useEffect(() => {
+    getAllUsers();
   }, [token]);
 
   useEffect(() => {
@@ -162,6 +193,7 @@ export default function Users() {
 
   return (
     <div className="flex flex-col gap-1 rtl font-iransans">
+       <AdminRegistration onUserAdded={getAllUsers} /> 
       <p className="text-2xl font-bold   bg-clip-text pb-2 border-b-2 border-customfour mr-10 mt-4 mb-6  w-[90%] mx-auto text-customfour">
         لیست کاربران
       </p>
