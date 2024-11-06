378e683a814b13b7ff8d383eeed2e1aadd674ce1
m.elyasi <maahsa.elyasi@gmail.com>
2024-11-06 16:49:42 +0330

    add modal general

---
+++ project/frontend/src/components/UI/Modal.jsx
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+
+export default function Modal({ isOpen, onClose, children }) {
+  const [showModal, setShowModal] = useState(false);
+
+  useEffect(() => {
+    if (isOpen) {
+      setTimeout(() => setShowModal(true), 200);
+    } else setShowModal(false);
+  }, [isOpen]);
+
+  return (
+    <>
+      {isOpen && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50 transition-opacity duration-300 ease-in-out">
+          {/* پس‌زمینه مدال */}
+          <div
+            className={`relative bg-myWhite p-6 rounded-lg shadow-lg w-[60%] xl:w-[40%]  transition-transform duration-300 ease-in-out transform ${
+              showModal ? "scale-100 opacity-100" : "scale-90 opacity-0"
+            }`}
+          >
+            {/* دکمه بستن */}
+            <button
+              onClick={onClose}
+              className="absolute top-3 right-3 text-black hover:text-greydark focus:outline-none text-xl"
+              aria-label="Close"
+            >
+              ✕
+            </button>
+            <div className="mt-2">{children}</div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
